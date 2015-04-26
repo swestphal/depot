62f4d31777c26a21583f46451226217ec0930d6c
Simone Westphal <simone.westphal@me.com>
2015-04-26 17:21:53 +0200

    catalog display

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  resources :carts
+
+  get 'store/index'
+
   resources :products
+
+  root 'store#index', as: 'store'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
